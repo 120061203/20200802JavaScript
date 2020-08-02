76f9eeb3ab5029a2a79986e11239e68561c30d7c
120061203 <ccssll120061203@gmail.com>
2020-08-02 23:26:52 +0800

    let variavle is not readable outside the forloop

--- myScript.js
+++ myScript.js
@@ -16,4 +16,13 @@ function printMessage(message, times) {
     }
     console.log("value is " + i);
 }
-console.log("i is " + i);
+// console.log("i is " + i);
+
+
+function letMessage(message, times) {
+    for (let i = 0; i < times; i++) {
+        console.log(message);
+    }
+    console.log("value of i is" + i);
+}
+letMessage("letMessage", 4);
