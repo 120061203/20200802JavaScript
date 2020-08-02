06ce99716e0dcee689965b403a5d2ad01b33a877
120061203 <ccssll120061203@gmail.com>
2020-08-03 01:10:36 +0800

    JS Example open present

--- event.js
+++ event.js
@@ -14,3 +14,13 @@ function onClickFunction() {
 }
 const button = document.querySelector('button');
 button.addEventListener('click', onClickFunction);
+/////////////////////////////////////////////////////
+//open present
+function openPresent() {
+    const image = document.querySelector('img');
+    image.src = 'https://media.giphy.com/media/27ppQUOxe7KlG/giphy.gif';
+    image.removeEventListener('click', openPresent);
+}
+
+const image = document.querySelector('img');
+image.addEventListener('click', openPresent);
