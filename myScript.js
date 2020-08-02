ddb42b1c7b3c3a7b4bdcbb34bcb925afec5d7db0
120061203 <ccssll120061203@gmail.com>
2020-08-02 23:40:04 +0800

    const variable is block scope not function scope

--- myScript.js
+++ myScript.js
@@ -1,13 +1,13 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "內容被松林修改了";
 }
-
+///////////////////////////////
 hoistedExercise();
 
 function hoistedExercise() {
     console.log("hoistedExercise Success");
 }
-
+////////////////////////////////////////////////
 printMessage("printMessage", 3);
 
 function printMessage(message, times) {
@@ -17,12 +17,20 @@ function printMessage(message, times) {
     console.log("value is " + i);
 }
 // console.log("i is " + i);
+////////////////////////////////////////////////////
 
-
-function letMessage(message, times) {
-    for (let i = 0; i < times; i++) {
-        console.log(message);
-    }
-    console.log("value of i is" + i);
+// function letMessage(message, times) {
+//     for (let i = 0; i < times; i++) {
+//         console.log(message);
+//     }
+//     console.log("value of i is" + i);
+// }
+// letMessage("letMessage", 4);
+///////////////////////////////////////////////////
+const x = 10;
+if (x > 0) {
+    //x = 5; //error const variable cannot reassign
+    const y = 10; //error y is not defined
 }
-letMessage("letMessage", 4);
+//console.log("x is " + x);
+console.log("y is " + y);
