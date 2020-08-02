9f5d07044b694e72a8de9b65aded806dae923ea8
120061203 <ccssll120061203@gmail.com>
2020-08-02 23:10:48 +0800

    hoisted exercise 程式提升

--- myScript.js
+++ myScript.js
@@ -1,3 +1,9 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "內容被松林修改了"
+    document.getElementById("demo").innerHTML = "內容被松林修改了";
+}
+
+hoistedExercise();
+
+function hoistedExercise() {
+    console.log("hoistedExercise Success");
 }
